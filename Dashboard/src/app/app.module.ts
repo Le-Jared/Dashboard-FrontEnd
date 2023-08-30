909b5b91f40d1e37de9b88e14797e0c81f857dcd
jared.lee <jared.lee.zhen.yan@gmail.com>
2023-08-30 22:10:49 +0800

    Highchart

--- Dashboard/src/app/app.module.ts
+++ Dashboard/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { HighchartsChartModule } from 'highcharts-angular';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
@@ -14,7 +16,8 @@ import { Chart1Component } from './chart1/chart1.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HighchartsChartModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Dashboard/src/app/chart1/chart1.component.ts
+++ Dashboard/src/app/chart1/chart1.component.ts
@@ -1,10 +1,80 @@
 import { Component } from '@angular/core';
+import * as Highcharts from 'highcharts';
 
 @Component({
-  selector: 'app-chart1',
-  templateUrl: './chart1.component.html',
-  styleUrls: ['./chart1.component.scss']
+   selector: 'app-chart1',
+   templateUrl: './chart1.component.html',
+   styleUrls: ['./chart1.component.scss']
 })
 export class Chart1Component {
-
+   public Highcharts = Highcharts;
+   public chartOptions: Highcharts.Options = {  
+      chart: {
+         type: 'column'
+      },
+      title: {
+         text: ''
+      },
+      legend: {
+         layout: 'vertical',
+         align: 'left',
+         verticalAlign: 'top',
+         x: 250,
+         y: 100,
+         floating: true,
+         borderWidth: 1,
+         backgroundColor: (
+           (Highcharts.theme as any)?.legendBackgroundColor || '#FFFFFF'
+         ),
+         shadow: true
+       },       
+      xAxis:{
+         categories: ['Africa', 'America', 'Asia', 'Europe', 'Oceania'], title: {
+            text: null
+         } 
+      },
+      yAxis : {
+         min: 0,
+         title: {
+            text: 'Population (millions)',
+            align: 'high'
+         },
+         labels: {
+            overflow: 'justify'
+         }
+      },
+      tooltip : {
+         valueSuffix: ' millions'
+      },
+      plotOptions : {
+         column: {
+            dataLabels: {
+               enabled: true
+            }
+         },
+         series: {
+            stacking: 'normal'
+         }
+      },
+      credits:{
+         enabled: false
+      },
+      series: [
+         {
+            type: 'column',
+            name: 'Year 1800',
+            data: [107, 31, 635, 203, 2]
+         }, 
+         {
+            type: 'column',
+            name: 'Year 1900',
+            data: [133, 156, 947, 408, 6]
+         }, 
+         {  
+            type: 'column',
+            name: 'Year 2008',
+            data: [973, 914, 4054, 732, 34]      
+         }
+      ]
+   };
 }
